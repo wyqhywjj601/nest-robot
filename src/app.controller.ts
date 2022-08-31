09c7dd11f33836faf039dbb4cb7a0c8f11c2927f
Weijunjie <912590051@qq.com>
2022-08-31 09:57:35 +0800

    feat: http封装

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
   @Post('wechat')
   wechat(@Req() request: Request): object {
     return this.appService.wechat(request);
   }
+  
 }
